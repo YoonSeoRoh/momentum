38c2e43742de02901e2e5db7577057a33814efc0
YoonSeoRoh <yoonseo1997@naver.com>
2022-01-25 14:55:34 +0900

    Third Commit

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,18 @@
 //사용자의 위치와 날씨를 제공
+const API_KEY = "22833186ee541cba36e37bea348d73a8";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
